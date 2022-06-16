e1ff19a80df51105d0e9bf23193f72849b1e7fea
Johan <57840332+johannasendi@users.noreply.github.com>
2022-06-16 17:48:51 +0800

    make it better

--- resources/views/layouts/_app-dashboard.blade.php
+++ resources/views/layouts/_app-dashboard.blade.php
@@ -18,22 +18,22 @@
    <link rel="shortcut icon" href="{{asset('/assets/img/logos.png')}}">
 
    <!-- Plugins css -->
-   <link href="{{asset('/vendor/assets/libs/mohithg-switchery/switchery.min.css')}}" rel="stylesheet" type="text/css" />
-   <link href="{{asset('/vendor/assets/libs/multiselect/css/multi-select.css')}}" rel="stylesheet" type="text/css" />
-   <link href="{{asset('/vendor/assets/libs/select2/css/select2.min.css')}}" rel="stylesheet" type="text/css" />
-   <link href="{{asset('/vendor/assets/libs/selectize/css/selectize.bootstrap3.css')}}" rel="stylesheet" type="text/css" />
-   <link href="{{asset('/vendor/assets/libs/bootstrap-select/css/bootstrap-select.min.css')}}" rel="stylesheet" type="text/css" />
-   <link href="{{asset('/vendor/assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css')}}" rel="stylesheet" type="text/css" />
+   <link href="{{asset('/admin/assets/libs/mohithg-switchery/switchery.min.css')}}" rel="stylesheet" type="text/css" />
+   <link href="{{asset('/admin/assets/libs/multiselect/css/multi-select.css')}}" rel="stylesheet" type="text/css" />
+   <link href="{{asset('/admin/assets/libs/select2/css/select2.min.css')}}" rel="stylesheet" type="text/css" />
+   <link href="{{asset('/admin/assets/libs/selectize/css/selectize.bootstrap3.css')}}" rel="stylesheet" type="text/css" />
+   <link href="{{asset('/admin/assets/libs/bootstrap-select/css/bootstrap-select.min.css')}}" rel="stylesheet" type="text/css" />
+   <link href="{{asset('/admin/assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css')}}" rel="stylesheet" type="text/css" />
 
    <!-- App css -->
-    <link href="{{asset('/vendor/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
-    <link href="{{asset('/vendor/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
+    <link href="{{asset('/admin/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
+    <link href="{{asset('/admin/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
 
-    <link href="{{asset('/vendor/assets/css/bootstrap-dark.min.css')}}" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" />
-    <link href="{{asset('/vendor/assets/css/app-dark.min.css')}}" rel="stylesheet" type="text/css" id="app-dark-stylesheet" />
+    <link href="{{asset('/admin/assets/css/bootstrap-dark.min.css')}}" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" />
+    <link href="{{asset('/admin/assets/css/app-dark.min.css')}}" rel="stylesheet" type="text/css" id="app-dark-stylesheet" />
 
     <!-- icons -->
-    <link href="{{asset('/vendor/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('/admin/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet" />
 
@@ -70,23 +70,23 @@
 
 
     <!-- Vendor js -->
-    <script src="{{asset('/vendor/assets/js/vendor.min.js')}}"></script>
+    <script src="{{asset('/admin/assets/js/vendor.min.js')}}"></script>
 
-    <script src="{{asset('/vendor/assets/libs/selectize/js/standalone/selectize.min.js')}}"></script>
-    <script src="{{asset('/vendor/assets/libs/mohithg-switchery/switchery.min.js')}}"></script>
-    <script src="{{asset('/vendor/assets/libs/multiselect/js/jquery.multi-select.js')}}"></script>
-    <script src="{{asset('/vendor/assets/libs/select2/js/select2.min.js')}}"></script>
-    <script src="{{asset('/vendor/assets/libs/jquery-mockjax/jquery.mockjax.min.js')}}"></script>
-    <script src="{{asset('/vendor/assets/libs/devbridge-autocomplete/jquery.autocomplete.min.js')}}"></script>
-    <script src="{{asset('/vendor/assets/libs/bootstrap-select/js/bootstrap-select.min.js')}}"></script>
-    <script src="{{asset('/vendor/assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js')}}"></script>
-    <script src="{{asset('/vendor/assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js')}}"></script>
+    <script src="{{asset('/admin/assets/libs/selectize/js/standalone/selectize.min.js')}}"></script>
+    <script src="{{asset('/admin/assets/libs/mohithg-switchery/switchery.min.js')}}"></script>
+    <script src="{{asset('/admin/assets/libs/multiselect/js/jquery.multi-select.js')}}"></script>
+    <script src="{{asset('/admin/assets/libs/select2/js/select2.min.js')}}"></script>
+    <script src="{{asset('/admin/assets/libs/jquery-mockjax/jquery.mockjax.min.js')}}"></script>
+    <script src="{{asset('/admin/assets/libs/devbridge-autocomplete/jquery.autocomplete.min.js')}}"></script>
+    <script src="{{asset('/admin/assets/libs/bootstrap-select/js/bootstrap-select.min.js')}}"></script>
+    <script src="{{asset('/admin/assets/libs/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js')}}"></script>
+    <script src="{{asset('/admin/assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js')}}"></script>
 
     <script src="https://kit.fontawesome.com/b00263187e.js" crossorigin="anonymous"></script>
     <!-- App js -->
-    <script src="{{asset('/vendor/assets/js/app.min.js')}}"></script>
+    <script src="{{asset('/admin/assets/js/app.min.js')}}"></script>
 
-    <script src="{{asset('/vendor/assets/js/pages/dashboard-1.init.js')}}"></script>
+    <script src="{{asset('/admin/assets/js/pages/dashboard-1.init.js')}}"></script>
 
     @include('sweetalert::alert')
 
